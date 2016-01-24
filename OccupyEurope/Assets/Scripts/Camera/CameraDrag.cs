b6c477ba9102720cdd3e9ff16c92098a30b5be75
Andrei Serban <noskillz05@gmail.com>
2016-01-24 19:18:31 +0200

    Initial with Europe map and pannable camera.

---
+++ OccupyEurope/Assets/Scripts/Camera/CameraDrag.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDrag : MonoBehaviour
+{
+    public float dragSpeed;
+    private Vector3 origin;
+    private GUIContent devGui;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            origin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0))
+        {
+            return;
+        }
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - origin);
+        Vector3 move = new Vector3(-pos.x * dragSpeed, -pos.y * dragSpeed, 0);
+
+        transform.Translate(move);
+    }
+
+
+    void OnGUI()
+    {
+        if (devGui == null)
+        {
+            devGui = new GUIContent();
+            return;
+        }
+        devGui.text = 
+            "Cur: " + Input.mousePosition + "\n" +
+            "Origin: " + origin;
+        GUI.Box(new Rect(10f, 10f, 250f, 100f), devGui);
+    }
+}
---
+++ OccupyEurope/Assets/Scripts/MapScript.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Collections;
+
+public class MapScript : MonoBehaviour
+{
+    private GUIContent devGui;
+    private SpriteRenderer map;
+    private Vector3 initialMousePosition;
+    private float mouseTravelDistance;
+    public float mouseTravelIgnore;
+    public float mapMoveMultiplier;
+
+    // Use this for initialization
+    public void Start()
+    {
+        devGui = new GUIContent(string.Empty);
+        map = FindObjectOfType<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        OnMapClick();
+    }
+
+    public void OnGUI()
+    {
+        //GUI.Box(new Rect(10f, 10f, 300f, 100f), devGui);
+        //devGui.text = string.Empty;
+        //devGui.text += "Pos: " + Input.mousePosition;
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    devGui.text += "\nClicked at: " + Input.mousePosition;
+        //    initialMousePosition = Input.mousePosition;
+        //}
+
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    devGui.text += ("\nReleased at: " + Input.mousePosition);
+
+        //    mouseTravelDistance = Vector3.Distance(initialMousePosition, Input.mousePosition);
+
+        //    Debug.Log(mouseTravelDistance);
+
+        //    if (mouseTravelDistance > mouseTravelIgnore)
+        //    {
+        //        Debug.Log("Camera should be moving.\nMouse travel distance should be " + mouseTravelDistance * mapMoveMultiplier);
+        //        map.transform.Translate(mouseTravelDistance * mapMoveMultiplier, mouseTravelDistance * mapMoveMultiplier, mouseTravelDistance * mapMoveMultiplier);
+        //    }
+        //}
+    }
+
+    private void OnMapClick()
+    {
+
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+
+        //    var initialPosition = Input.mousePosition;
+        //    devGui.text = "Mouse down" + "\n" + "Pos: " + Input.mousePosition;
+
+        //    if (HasMouseMoved())
+        //    {
+        //        devGui.text += "\n" + HasMouseMoved();
+        //        map.gameObject.transform.Translate(1f, 1f, 1f);
+        //    }
+
+        //}
+
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    devGui.text = "Mouse up" + "\n" + "Pos: " + Input.mousePosition;
+        //}
+    }
+}
